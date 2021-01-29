f29f60bbf5e66f51a35f09a0dcc119431e022ba7
hannah <Hannah Weller>
2021-01-29 17:27:09 -0500

    location specification for absorbLayer

--- R/absorbLayer.R
+++ R/absorbLayer.R
@@ -1,15 +1,20 @@
 #' Absorb a layer into its surrounding color patches
 #'
-#' Every component of a layer which satisfies a size condition is 'absorbed' into
-#' the color map by switching its color to that of the color patch with which
-#' it shares the longest border. Useful for dealing with reflections, gloss,
-#' speckles, small features, etc.
-#'
+# Every component of a layer which satisfies a size and location condition is
+# 'absorbed' into the color map by switching its color to that of the color
+# patch with which it shares the longest border. Useful for dealing with
+# reflections, gloss, speckles, small features, etc.
+
 #' @param recolorize_obj A `recolorize` object.
 #' @param layer_idx The numeric index of the layer to absorb.
 #' @param size_condition A condition for determining which components to absorb,
-#' written as a function. The default (`function(x) x <= 100`) will only
-#' affect components that are 100 pixels or fewer in size.
+#' written as a function. The default (`function(l) l <= Inf`) will affect
+#' all components, since they all have fewer than infinity pixels.
+#' @param x_range,y_range The rectangular bounding box (as proportions of the
+#'   image width and length) for selecting patches. Patches with at least
+#'   partial overlap are counted. Defaults (0-1) include the entire image.
+#'   See details.
+#' @param y_range
 #' @param highlight_color Color for highlighting the affected layer.
 #' @param plotting Logical. Plot results?
 #'
@@ -17,38 +22,88 @@
 #'
 #' @details This function works by splitting a layer into spatially distinct
 #' 'components' using [imager::split_connected]. A contiguous region of pixels
-#' is considered a single component. Any components whose size satisfies the condition
-#' provided in the `size_condition` argument is then 'absorbed' by the surrounding
-#' color patches. This works by counting up all of the pixels that border that
-#' component, and switching the color of that component to the patch with which
-#' it shares the longest border.
+#' is considered a single component. Only components which satisfy
+#' both the `size_condition` and the location condition (specified via `x_range`
+#' and `y_range`) are absorbed, so you can be target specific regions with
+#' (ideally) a minimum of fuss.
+#'
+#' The `size_condition` is passed as a function which must have a logical
+#' vector output (`TRUE` and `FALSE`) when applied to a vector of sizes.
+#' Usually this will be some combination of greater and less than statements,
+#' combined with logical operators like `&` and `|`. For example,
+#' `size_condition = function(x) x > 100 | x < 10` would affect components of
+#' greater than 100 pixels and fewer than 10 pixels, but not those with 10-100
+#' pixels.
+#'
+#' The `x_range` and `y_range` values set the bounding box of a rectangular
+#' region as proportions of the image axes, with the origin (0, 0) in the bottom
+#' left corner. Any patch which has at least partial overlap with this bounding
+#' box will be considered to satisfy the condition. When selecting this region,
+#' it can be helpful to plot a grid on the image first to narrow down an
+#' approximate region (see examples).
+#'
 #'
 #' @seealso [editLayers] for editing layers using morphological operations;
 #' [thresholdRecolor] for re-fitting the entire image without minor colors.
 #'
 #' @examples
-#' img <- system.file("extdata/ephippigera.png", package = "recolorize"); img
-#' fit1 <- recolorize(img, bins = 3)
-#' fit2 <- recluster(fit1)
-#' fit3 <- absorbLayer(recolorize_obj = fit3, layer_idx = 4,
-#'                    size_condition = function(x) x <= 30 & x > 0,
-#'                    highlight_color = "cyan")
+#' img <- system.file("extdata/fulgidissima.png", package = "recolorize")
 #'
+#' # get an initial fit using recolorize + recluster:
+#' fit1 <- recolorize(img, bins = 3, plotting = FALSE)
+#' fit2 <- recluster(fit1, similarity_cutoff = 65)
+#' # this looks okay, but the brown patch (3) has some speckling
+#' # in the upper right elytron due to reflection, and the orange
+#' # patch (4) has the same issue
 #'
+#' # the brown patch is easier to deal with, since size thresholding alone is
+#' # sufficient; we want to leave the stripes intact, so we'll absorb components
+#' # that are 50-250 pixels OR fewer than 20 pixels (to get the tiny speckles),
+#' # leaving the eyes intact
+#' fit3 <- absorbLayer(fit2, layer_idx = 3,
+#'                     size_condition = function(x) x <= 250 &
+#'                       x >= 50 |
+#'                       x < 20,
+#'                     highlight_color = "cyan")
+#'
+#' # what about the orange speckles? this is more difficult, because
+#' # we want to retain the border around the brown stripes, but those patches
+#' # are quite small, so size thresholding won't work:
+#' fit_bad <- absorbLayer(fit3, layer_idx = 4,
+#'                        size_condition = function(x) x < 25)
+#'
+#' # but we just want to target pixels in that one region, so we can first
+#' # determine a bounding box for it by plotting a grid:
+#' plotImageArray(fit3$recolored_img)
+#' axis(1, line = 3); axis(2, line = 1)
+#' abline(v = seq(0, 1, by = 0.1),
+#'        h = seq(0, 1, by = 0.1),
+#'        col = grey(0.2),
+#'        lty = 2)
+#' # x-axis range: 0.5-0.7
+#' # y-axis range: 0.55-0.75
+#' # let's try it:
+#' fit4 <- absorbLayer(fit3, layer_idx = 4,
+#'                     size_condition = function(x) x < 100,
+#'                     x_range = c(0.5, 0.7),
+#'                     y_range = c(0.55, 0.75),
+#'                     highlight_color = "yellow")
+#' # looks pretty good!
 #' @export
 absorbLayer <- function(recolorize_obj,
-                          layer_idx,
-                          size_condition = function(x) x <= 100,
-                          highlight_color = "yellow",
-                          lwd = 0.25,
-                          plotting = TRUE) {
+                        layer_idx,
+                        size_condition = function(s) s <= 100,
+                        x_range = c(0, 1),
+                        y_range = c(0, 1),
+                        highlight_color = "yellow",
+                        lwd = 0.25,
+                        plotting = TRUE) {
   # get object layer
   layers <- splitByColor(recolorize_obj, plot_method = "none")
   layer <- layers[[layer_idx]]
 
   # convert to an imager pixset for splitting
   px <- imager::as.pixset(imager::as.cimg(layer) > 0)
-  px <- imager::imrotate(px, 90)
 
   # split separate components
   layer_split <- imager::split_connected(px)
@@ -56,13 +111,34 @@ absorbLayer <- function(recolorize_obj,
   # get component sizes
   component_sizes <- unlist(lapply(layer_split, sum))
 
-  # find which components satisfy the condition
+  # find which components satisfy the size condition
   condition_met <- which(ifelse(size_condition(component_sizes), TRUE, FALSE))
+
+  # location condition - bounding box
+  # get bounding box (in pixel coordinates) of each component:
+  component_bbox <- lapply(layer_split, function(l) apply(which(l[ , , 1, 1],
+                                              arr.ind = TRUE), 2,
+                                        range))
+
+  # convert to fraction of image dimensions:
+  imdim <- dim(recolorize_obj$original_img)[1:2]
+  norm_matrix <- matrix(imdim, nrow = 2, ncol = 2, byrow = TRUE)
+  component_bbox_norm <- lapply(component_bbox,
+                                function(b) round(b / norm_matrix, 3))
+  # important - remember that imager rotates images 90 degrees (argh),
+  # so y-axis is rows and x-axis is columns
+  y_range <- 1 - y_range # also the y-axis is flipped
+  bbox_condition_met <- lapply(component_bbox_norm,
+                               function(b) any(any(b[ , 1] >= y_range[2]) &
+                                               any(b[ , 1] <= y_range[1])) &
+                                           any(any(b[ , 2] >= x_range[1]) &
+                                               any(b[ , 2] <= x_range[2])))
+  condition_met <- intersect(condition_met,
+                             which(unlist(bbox_condition_met)))
 
   # make a color center map from the pixel assignments (this will make sense in
   # a bit)
   map <- imager::as.cimg(recolorize_obj$pixel_assignments)
-  map <- imager::imrotate(map, 90)
 
   # for every component that meets the size condition:
   for (i in condition_met) {
@@ -103,12 +179,20 @@ absorbLayer <- function(recolorize_obj,
   }
 
   # switch the new patch map in
-  map2 <- recolorize:::cimg_to_array(imager::imrotate(map, -90))
+  map2 <- recolorize:::cimg_to_array(map)
   recolorize_obj$pixel_assignments <- map2
 
   # and make the image
   new_img <- constructImage(map2,
                             recolorize_obj$centers)
+
+  # then, get new sizes (minus background)
+  recolorize_obj$sizes <- table(map2)[-1]
+
+  # if we completely eliminated a patch, remove it from the color centers
+  if (!any(map2 == layer_idx)) {
+    recolorize_obj$centers <- recolorize_obj$centers[-layer_idx, ]
+  }
 
   # plot if we're plotting
   if (plotting) {
@@ -118,9 +202,9 @@ absorbLayer <- function(recolorize_obj,
     layer_px <- imager::as.pixset(imager::add(components) > 0)
     px_bound <- imager::boundary(imager::grow(layer_px, 1))
     old_img <- array_to_cimg(recolorize_obj$recolored_img)
-    highlight_img <- imager::colorise(imager::imrotate(old_img, 90),
+    highlight_img <- imager::colorise(old_img,
                      px_bound, col = highlight_color)
-    highlight_img <- cimg_to_array(imager::imrotate(highlight_img, -90))
+    highlight_img <- cimg_to_array(highlight_img)
 
     # plot highlighted
     layout(matrix(1:3, 1), widths = c(0.4, 0.4, 0.2))
@@ -137,13 +221,6 @@ absorbLayer <- function(recolorize_obj,
   # first, swap out the new image
   recolorize_obj$recolored_img <- new_img
 
-  # then, get new sizes (minus background)
-  recolorize_obj$sizes <- table(map2)[-1]
-
-  # if we completely eliminated a patch, remove it from the color centers
-  if (!any(map2 == layer_idx)) {
-    recolorize_obj$centers <- recolorize_obj$centers[-layer_idx, ]
-  }
   return(recolorize_obj)
 
 }
