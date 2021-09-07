1eda48e32a3632d76e0b9b61326c6f939637e743
hannah_weller@brown.edu <hannah_weller@brown.edu>
2021-09-07 13:26:43 -0400

    added recolorize_to_patternize function

---
+++ R/recolorize_to_patternize.R
@@ -0,0 +1,57 @@
+#' Convert a recolorize object to a raster object
+#'
+#' Convert from a `recolorize` object to a list of RasterLayer objects, the
+#' format required by the `patternize` package. Note that most of the downstream
+#' `patternize` functions that require lists of RasterLayer objects mostly
+#' require lists of these lists, so you will probably need to use this function
+#' on a list of `recolorize` objects.
+#'
+#' @param recolorize_obj A `recolorize` object.
+#' @param return_background Logical.
+#'
+#' @details Note that this function does not retain the colors of the layers --
+#'   you won't be able to convert back to a recolorize object from this object.
+#'
+#' @return A list of RasterLayer objects, one per color class.
+#'
+#' @examples
+#'
+#' # fit recolorize object:
+#' img <- system.file("extdata/ephippigera.png", package = "recolorize")
+#' rc <- recolorize2(img)
+#'
+#' # convert to a raster list:
+#' as_raster_list <- recolorize_to_patternize(rc)
+#'
+#' # we can plot the separate layers:
+#' layout(matrix(1:8, nrow = 2))
+#' lapply(as_raster_list, raster::plot)
+#' @export
+recolorize_to_patternize <- function(recolorize_obj,
+                                     return_background = FALSE) {
+
+  # convert to a raster first
+  r <- raster::raster(recolorize_obj$pixel_assignments)
+
+  # iterate through unique components
+  l <- 1:nrow(recolorize_obj$centers)
+
+  if (return_background) {
+    l <- c(0, l)
+  }
+
+  # make a list to store the layers
+  layer_list <- vector("list", length = length(l))
+
+  # for every layer...
+  for (i in 1:length(l)) {
+
+    # store all the coordinates equal to that layer
+    layer_list[[i]] <- r == l[i]
+  }
+
+  # voila
+  return(layer_list)
+
+}
+
